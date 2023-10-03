368aab8f1ae1f0b5ed7fa1a577438b4afcd723aa
Pham Thanh Nghia <phamthanhnghia.it@gmail.com>
2023-10-03 16:04:00 +0700

    Add UserSeeder

---
+++ laravel/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+        ]);
+        User::factory()->count(10)->create();
+    }
+}
