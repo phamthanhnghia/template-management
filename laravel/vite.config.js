06122bc19097ac880bf08ae0fb862359e7274243
Pham Thanh Nghia <phamthanhnghia.it@gmail.com>
2023-10-06 16:05:58 +0700

    Setup Laravel Breeze React

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        reactRefresh()
+        react(),
     ],
 });
